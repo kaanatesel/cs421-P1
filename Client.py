d26aac345e4cceb8bd90c364ebfe6ccba5437ee2
Kaan Ateşel <kaanatesel99@gmail.com>
2022-11-03 23:52:38 +0300

    update method

--- Client.py
+++ Client.py
@@ -1,7 +1,8 @@
 import socket
-import json
-HOST='127.0.0.1'
-PORT=6000
+import io
+
+HOST = '127.0.0.1'
+PORT = 6000
 NEWLINE = "\r\n"
 USERNAME = "bilkentstu"
 PASS = "cs421f2022"
@@ -9,25 +10,44 @@ ENCODING = "ascii"
 
 
 class Client:
-    def __int__(self):
-        pass
+
+    def __init__(self):
+        self.version = 3
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect()
+        self.OK = 'OK'
+        self.INVALID = 'INVALID'
+
     def connect(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST,PORT))
-            name = f'USER {USERNAME}'
-            password = f'PASS {PASS}'
-            s.send(bytes(name + NEWLINE, ENCODING))
-            s.send(bytes(password + NEWLINE, ENCODING))
+        self.s.connect((HOST, PORT))
+        name = f'USER {USERNAME}'
+        password = f'PASS {PASS}'
+        self.s.send(bytes(name + NEWLINE, ENCODING))
+        self.s.send(bytes(password + NEWLINE, ENCODING))
+        print(f'connection status = {self.s.recv(1024).decode()}')
 
-            print(s.recv(1024).decode())
+    def update(self):
+        cmd = f'UPDT {self.version}'
+        self.s.send(bytes(cmd + NEWLINE, ENCODING))
+        response = self.s.recv(1024).decode()
+        status, message = self.receive_response(response)
+        if(status == self.OK):
+            self.version = int(message)
+        else:
+            print("ERROR, cannot update")
 
+    def write(self):
+        pass
 
-            s.close()
+    def exit(self):
+        self.s.close()
+
+    def receive_response(self, res):
+        chunks = res.split(' ')
+
+        print("Command received:", chunks)
+        return chunks[0], chunks[1]
 
 
 cl1 = Client()
-cl1.connect()
-
-
-
-
+cl1.update()
