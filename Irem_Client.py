64d18d5a0208d85b8a53207b4eb11aeb41313ed6
Kaan Ateşel <kaanatesel99@gmail.com>
2022-11-04 20:15:18 +0300

    birlesmis

--- Irem_Client.py
+++ Irem_Client.py
@@ -3,7 +3,7 @@ import os
 import time
 
 HOST = '127.0.0.1'
-PORT = 6000
+PORT = 60000
 NEWLINE = "\r\n"
 USERNAME = "bilkentstu"
 PASS = "cs421f2022"
@@ -13,25 +13,32 @@ ENCODING = "ascii"
 class Client:
 
     def __init__(self):
-        self.version = 0
+        self.version = -1
 
         #self.user_prime(nickname)
         self.OK = 'OK'
         self.INVALID = 'INVALID'
         #self.name = name
-
-    def user_prime(self, nickname):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((HOST, PORT))
-        cmd = f'USER {USERNAME} {nickname}'
-        self.s.send(bytes(nickname + NEWLINE, ENCODING))
-        print(f'connection status = {self.s.recv(1024).decode()}')  # ne yazdırcaz emin olamadım
+        self.name = ''
+        self.password = ''
 
-    def password(self, pass_user):
-        self.s.connect((HOST, PORT))
-        password = f'PASS {PASS}{pass_user}'
+    def user_prime(self, name):
+        self.name = name
+
+    def user_password(self, pass_user):
+        self.password = pass_user
+
+    def connect(self):
+        #name = f'USER {self.name}'
+        #password = f'PASS {self.password}'
+        name = f'USER {USERNAME}'
+        password = f'PASS {PASS}'
+        self.s.send(bytes(name + NEWLINE, ENCODING))
         self.s.send(bytes(password + NEWLINE, ENCODING))
-        print(f'connection status = {self.s.recv(1024).decode()}')#ne yazdırcaz emin olamadım
+        print(f'connection status = {self.s.recv(1024).decode()}')
+        self.exit()
 
     def update(self):
         cmd = f'UPDT {self.version}\r\n'
@@ -39,21 +46,23 @@ class Client:
         data = self.s.recv(1024).decode()
         status, version, txt = self.receive_update_response(data)
         if not data or status != self.OK or version == '-1':
-            print("Error while loading txt file.")
+            print(data)
         else:
             self.version = int(version)
             with open(f"{self.name}.txt", "w+") as txt_file:
                 for line in txt:
                     txt_file.write("".join(line) + "\n")
             print(f"File is updated to version {self.version}. For {self.name}")
-
+        self.exit()
     def append(self, msg):
+        self.connect()
         self.update()
         cmd = f'APND {self.version} {msg}\r\n'
         self.s.send(bytes(cmd + NEWLINE, ENCODING))
         res = self.s.recv(1024).decode()
         resEdited = self.receive_response(res)
         print(resEdited)
+        self.exit()
 
     def write(self,line, msg):
         self.update()
@@ -96,36 +105,38 @@ cl1 = Client()
 ##cl1.update()
 ##cl1.exit()
 
-print("Please enter the number to choose which protocol you want to proceed with?\n\
-      1. USER\n\
-      2. PASS\n\
-      3. WRITE\n\
-      4. APPEND\n\
-      5. UPDATE\n\
-      6. EXIT")
+while True:
+    print("Please enter the number to choose which protocol you want to proceed with?\n\
+          0. CONNECT\n\
+          1. USER\n\
+          2. PASS\n\
+          3. WRITE\n\
+          4. APPEND\n\
+          5. UPDATE\n\
+          6. EXIT")
 
-protocol = int(input())
-
-if (protocol == 1):
-    user_inp = input("Please enter your username.")
-    cl1.user_prime(user_inp)
-
-elif protocol == 2:
-    pass_inp = input ("Please enter your password.")
-    cl1.password(pass_inp)
-
-elif protocol == 3:
-    line_num= input ("Please enter the line number.")
-    message = input("Please enter your message.")
-    cl1.write(line_num, message)
-elif protocol == 4:
-    app_msg= input ("Please enter your message.")
-    cl1.append(app_msg)
-elif protocol == 5:
-    cl1.update()
-    print("The server is updated.")
-elif protocol == 6:
-    print("Exiting...")
-    cl1.exit()
-else:
-    print("Invalid. Please try again.")
+    protocol = int(input())
+    if(protocol == 0):
+        cl1.connect()
+    elif (protocol == 1):
+        user_inp = input("Please enter your username.")
+        cl1.user_prime(user_inp)
+    elif protocol == 2:
+        pass_inp = input("Please enter your password.")
+        cl1.user_password(pass_inp)
+    elif protocol == 3:
+        line_num = input("Please enter the line number.")
+        message = input("Please enter your message.")
+        cl1.write(line_num, message)
+    elif protocol == 4:
+        app_msg = input("Please enter your message.")
+        cl1.append(app_msg)
+    elif protocol == 5:
+        cl1.update()
+        print("The server is updated.")
+    elif protocol == 6:
+        print("Exiting...")
+        cl1.exit()
+        break
+    else:
+        print("Invalid. Please try again.")
